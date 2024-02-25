735e9b9511225a810a219e216bfeab6cf16832ec
madhukeshwarh <57562468+madhukeshwarh@users.noreply.github.com>
2024-02-25 18:47:28 +0530

    Add files via upload

---
+++ madlibs/madlibs.py
@@ -0,0 +1,19 @@
+
+def madlib():
+    food = input('food name: ')
+    name= input('enter a name: ')
+    place = input('enter a place name: ')
+    animals= input('enter a animal name : ')
+    profession = input('enter a profession name: ')
+    things = input('enter a thing name: ')
+    cloth = input('enter a piece of cloth name: ')
+    verb = input('enter a verb in ing form: ')
+    
+    madlib=f"say {food }, the photographer said as the camera flashed!  {name} and I had gone to {place} to get our photos taken today.\
+    The first photo we really wanted was a picture of us dressed as {animals} pretending to be a {profession}.when we saw the second photo,\
+    it was exactly what I wanted. We both looked like {things} wearing {cloth} and  {verb } --exactly what I had in mind"
+    print(madlib)
+madlib()
+
+    
+
